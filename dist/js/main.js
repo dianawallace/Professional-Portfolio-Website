6b3c50732cfe3c2483c0481a310bbcec444b0065
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-10-30 19:29:51 -0600

    created select DOM items variables and changed secondary color

---
+++ dist/js/main.js
@@ -0,0 +1,6 @@
+// Select DOM Items
+const menuBtn = document.querySelector(".btn-menu");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-items");
