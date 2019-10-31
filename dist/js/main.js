95c8ade452a964bc97e5b40db56e8d2abd96c00d
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-10-30 19:36:31 -0600

    set initial state menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,4 +3,20 @@ const menuBtn = document.querySelector(".btn-menu");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".nav-items");
+const navItem = document.querySelectorAll(".nav-items");
+
+// Set Intiial State of Menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+  } else {
+  }
+}
